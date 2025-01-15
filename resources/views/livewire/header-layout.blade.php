ec2543c9aad6c79eae497cd491472f1a883e840d
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-01-15 21:17:51 +0700

    feat: setup sidebar and header palette

--- resources/views/livewire/header-layout.blade.php
+++ resources/views/livewire/header-layout.blade.php
@@ -1,24 +1,23 @@
 <div
-    class="fixed w-full z-50 bg-white dark:bg-[#252525] text-gray-800 dark:text-gray-50 flex justify-end lg:justify-between items-center px-4 py-4 shadow-md">
+    class="fixed w-full z-50 bg-main-bg dark:bg-[#252525] text-main-text dark:text-dark-main-text flex justify-end lg:justify-between items-center px-4 py-4 shadow-md">
     <div class="hidden lg:flex items-center">
         <div
             class="bg-gradient-to-r from-[#F14A00] via-[#F93827] to-[#FF9D23] inline-block text-transparent bg-clip-text">
             <i class="fa-solid fa-seedling text-2xl"></i>
         </div>
-        <h1 class="text-tertiary-text dark:text-gray-50 font-semibold text-xl ml-2 font-sans">
+        <h1 class="text-tertiary-text font-semibold text-xl ml-2 font-sans">
             Pos <span class="text-fourtiary-text">App.</span>
         </h1>
     </div>
-    <div class="relative flex items-center space-x-4">
+    <div x-data="{ isOpen: false }" class="relative flex items-center space-x-4">
         <!-- Profile Toggle Button -->
-        <button wire:click='profileToggle' class="focus:outline-none">
+        <button @click="isOpen = !isOpen" class="focus:outline-none">
             <i class="fa-solid fa-circle-user text-3xl"></i>
         </button>
 
         <!-- Dropdown Menu -->
-        @if ($isProfileButtonVisible)
-        <div
-            class="absolute top-10 right-0 mt-2 w-52 bg-white dark:bg-[#252525] text-gray-800 dark:text-gray-50 border dark:border-[#3c3c3c] rounded shadow-lg z-50">
+        <div x-show="isOpen" @click.away="isOpen = false"
+            class="absolute top-10 right-0 mt-2 w-52 bg-main-bg dark:bg-[#252525] text-main-text dark:text-dark-main-text border dark:border-[#3c3c3c] rounded shadow-lg z-50">
             <div class="py-2">
                 <div class="px-4 pt-1 pb-2">
                     <div class="flex items-center space-x-2 pl-1 py-1">
@@ -36,16 +35,14 @@
                 <div class="border-t border-gray-200/80 dark:border-[#3c3c3c] flex justify-center">
                     <div class="mx-4 my-2">
                         <div class="flex z-50 space-x-1">
-                            <div wire:click='profileToggle'
-                                class="rounded-md cursor-pointer hover:text-gray-500 px-7 py-2 duration-100"
+                            <div class="rounded-md cursor-pointer hover:text-tertiary-text px-7 py-2 duration-100"
                                 @click="darkMode= false"
-                                :class="darkMode ? 'text-gray-800 dark:text-gray-50' : 'bg-gray-100 text-green-500'">
+                                :class="darkMode ? 'text-main-text dark:text-gray-50' : 'bg-gray-100 text-tertiary-text'">
                                 <i class="fa-regular fa-lightbulb text-lg"></i>
                             </div>
-                            <div wire:click='profileToggle'
-                                class="rounded-md cursor-pointer hover:text-gray-500 px-7 py-2 duration-100"
+                            <div class="rounded-md cursor-pointer hover:text-tertiary-text px-7 py-2 duration-100"
                                 @click="darkMode= true"
-                                :class="darkMode ? 'bg-[#303030] text-green-500' : 'text-gray-800'">
+                                :class="darkMode ? 'bg-[#303030] text-tertiary-text' : 'text-main-text dark:text-dark-main-text'">
                                 <i class="fa-solid fa-lightbulb text-lg"></i>
                             </div>
                         </div>
@@ -55,16 +52,17 @@
                 {{-- <div class="border-t border-gray-200/80 dark:border-[#3c3c3c]">
                     <div class="mx-1 pt-2 pb-2">
                         <a href="{{ route('user-management.index') }}" wire:navigate
-                            class="hover:bg-gray-100 dark:hover:bg-[#373636] {{ $isSetting ? 'text-green-500 bg-gray-100 dark:bg-[#373636]' : 'text-gray-800 dark:text-gray-50' }} px-4 py-2 rounded-md flex items-center space-x-2 text-sm cursor-pointer">
+                            class="hover:bg-gray-100 dark:hover:bg-[#373636] {{ $isSetting ? 'text-green-500 bg-gray-100 dark:bg-[#373636]' : 'text-main-text dark:text-gray-50' }} px-4 py-2 rounded-md flex items-center space-x-2 text-sm cursor-pointer">
                             <i class="ri-user-settings-line text-xl"></i>
                             <span>Pengaturan</span>
                         </a>
                     </div>
                 </div> --}}
+
                 <div class="border-t border-gray-200/80 dark:border-[#3c3c3c]">
                     <div class="mx-1 pt-2">
                         <div wire:click='logout'
-                            class="hover:bg-gray-100 dark:hover:bg-[#373636] px-4 py-2 rounded-md flex items-center space-x-2 text-sm cursor-pointer">
+                            class="hover:bg-gray-100 dark:hover:bg-[#373636] px-4 py-2 rounded-md flex items-center space-x-2 text-sm text-main-text dark:text-dark-main-text font-semibold cursor-pointer">
                             <i class="ri-logout-circle-line text-xl"></i>
                             <span>Keluar</span>
                         </div>
@@ -72,6 +70,5 @@
                 </div>
             </div>
         </div>
-        @endif
     </div>
 </div>
--- resources/views/livewire/sidebar-toggle.blade.php
+++ resources/views/livewire/sidebar-toggle.blade.php
@@ -1,8 +1,8 @@
 <div x-data="{ isSidebarVisible: @entangle('isSidebarVisible') }">
     <!-- Toggle Button -->
     <button @click="isSidebarVisible = !isSidebarVisible"
-        class="lg:hidden p-2 fixed top-4 left-4 z-50 dark:text-gray-50">
-        <i class="ri-menu-2-line"></i>
+        class="lg:hidden p-2 fixed top-4 left-4 z-50 text-main-text dark:text-dark-main-text">
+        <i class="fa-solid fa-bars"></i>
     </button>
 
     <!-- Overlay -->
@@ -10,23 +10,20 @@
         class="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"></div>
 
     <!-- Sidebar -->
-    <div :class="isSidebarVisible ? 'translate-x-0' : '-translate-x-full'" {{--
-        class="bg-[#FAFAFA] dark:bg-[#1c1c1c] fixed top-0 left-0 min-h-screen w-72 duration-500 text-gray-100 px-3 z-40 pt-20 lg:pt-10 transform lg:translate-x-0">
-        --}}
-        class="bg-white dark:bg-[#1c1c1c] shadow-lg fixed top-0 left-0 min-h-screen w-[5.5rem] duration-500
+    <div :class="isSidebarVisible ? 'translate-x-0' : '-translate-x-full'" class="bg-main-bg dark:bg-[#252525] shadow-lg fixed top-0 left-0 min-h-screen w-[5.5rem] duration-500
         text-gray-100 px-3
         z-40
         pt-20 lg:pt-10 transform lg:translate-x-0">
         <div class="flex lg:hidden items-center mt-2">
             <img src="/img/Logo.png" alt="logo" class="w-7 h-8 ml-2" />
-            <h1 class="text-gray-800 dark:text-gray-50 font-semibold text-xl ml-2 font-sans">
+            <h1 class="text-main-text dark:text-dark-main-text font-semibold text-xl ml-2 font-sans">
                 Kasir.
             </h1>
         </div>
-        <div class="mt-10 flex flex-col gap-2 relative text-gray-800 dark:text-gray-50">
+        <div class="mt-10 flex flex-col gap-2 relative text-main-text dark:text-dark-main-text">
             @foreach ($sidebarMenu as $menu)
             <a href="{{ route($menu['route']) }}" wire:navigate
-                class="group flex justify-center items-center text-sm h-11 gap-3.5 font-medium p-2 hover:bg-[#f2f2f2] dark:hover:bg-[#252525] hover:text-green-500 dark:hover:text-green-500 rounded-md {{ request()->is($menu['request']) ? 'bg-[#f2f2f2] dark:bg-[#252525] text-green-500' : 'text-gray-800 dark:text-gray-50' }}">
+                class="group flex justify-center items-center text-sm h-11 gap-3.5 font-medium p-2 hover:bg-[#f2f2f2] dark:hover:bg-[#252525] hover:text-tertiary-text dark:hover:text-tertiary-text rounded-md {{ request()->is($menu['request']) ? 'bg-[#f2f2f2] dark:bg-[#252525] text-tertiary-text' : 'text-main-text dark:text-dark-main-text' }}">
                 <i class="{{ $menu['icon'] }} text-lg"></i>
                 {{-- <h2 class="whitespace-pre duration-300 capitalize">{{ $menu['name'] }}</h2> --}}
             </a>
