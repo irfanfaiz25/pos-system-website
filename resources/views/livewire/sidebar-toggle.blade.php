95a8593666a560701c47aa966bcee08e171cd864
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-01-24 20:16:57 +0700

    feat: add tooltip to sidebar menu

--- resources/views/livewire/sidebar-toggle.blade.php
+++ resources/views/livewire/sidebar-toggle.blade.php
@@ -12,9 +12,9 @@
     <!-- Sidebar -->
     <div :class="isSidebarVisible ? 'translate-x-0' : '-translate-x-full'"
         class="bg-main-bg dark:bg-[#252525] shadow-lg fixed top-0 left-0 min-h-screen w-[5.5rem] duration-500
-        text-gray-100 px-3
-        z-40
-        pt-20 lg:pt-10 transform lg:translate-x-0">
+text-gray-100 px-3
+z-40
+pt-20 lg:pt-10 transform lg:translate-x-0">
         <div class="flex lg:hidden items-center mt-2">
             <div
                 class="bg-gradient-to-r from-[#F14A00] via-[#F93827] to-[#FF9D23] inline-block text-transparent bg-clip-text">
@@ -24,12 +24,18 @@
                 Pos <span class="text-fourtiary-text">App.</span>
             </h1>
         </div>
+
+        {{-- Sidebar Menu --}}
         <div class="mt-10 flex flex-col gap-2 relative text-main-text dark:text-dark-main-text">
             @foreach ($sidebarMenu as $menu)
                 <a href="{{ route($menu['route']) }}" wire:navigate
                     class="group flex justify-center items-center text-sm h-11 gap-3.5 font-medium p-2 hover:bg-[#f2f2f2] dark:hover:bg-[#252525] hover:text-tertiary-text dark:hover:text-tertiary-text rounded-md {{ request()->is($menu['request']) ? 'bg-[#f2f2f2] dark:bg-[#252525] text-tertiary-text' : 'text-main-text dark:text-dark-main-text' }}">
                     <i class="{{ $menu['icon'] }} text-lg"></i>
-                    {{-- <h2 class="whitespace-pre duration-300 capitalize">{{ $menu['name'] }}</h2> --}}
+                    <!-- Floating Tooltip -->
+                    <span
+                        class="absolute left-full ml-2 px-2 py-1 bg-[#252525] dark:bg-[#f2f2f2] text-white dark:text-[#252525] text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                        {{ $menu['name'] }}
+                    </span>
                 </a>
             @endforeach
         </div>
