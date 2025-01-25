0bb6bd7ab4d0a5c94160d3d36e837f0028613eac
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-01-25 09:50:27 +0700

    fix: change the search icon and modify the search color

--- resources/views/content/transactions.blade.php
+++ resources/views/content/transactions.blade.php
@@ -33,16 +33,12 @@
                             class="w-full bg-main-bg dark:bg-dark-main-bg placeholder:text-slate-400 text-main-text text-sm border border-gray-200 dark:border-[#393939] rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-main-border/80 dark:focus:border-main-border/80 hover:border-gray-300 shadow-sm focus:shadow"
                             placeholder="Search ..." />
                         <button
-                            class="absolute top-1 right-1 flex items-center rounded bg-secondary-bg py-1 px-2.5 border border-transparent text-center text-sm text-dark-main-text transition-all shadow-smdisabled:pointer-events-none font-normal"
+                            class="absolute top-1 right-1 flex items-center space-x-1 rounded bg-secondary-bg/80 py-1 px-2.5 border border-transparent text-center text-sm text-dark-main-text transition-all shadow-smdisabled:pointer-events-none font-normal"
                             type="button" disabled>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="w-4 h-4 mr-2">
-                                <path fill-rule="evenodd"
-                                    d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
-                                    clip-rule="evenodd" />
-                            </svg>
-
-                            Search
+                            <i class="fa-solid fa-magnifying-glass text-sm"></i>
+                            <span>
+                                Search
+                            </span>
                         </button>
                     </div>
                 </div>
