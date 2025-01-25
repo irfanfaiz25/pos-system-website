f69f9a5cb07059e5a7a697a02ed4ae0eeb9d086e
irfanfaiz25 <ir.faiz2000@gmail.com>
2025-01-26 06:56:50 +0700

    fix: change image display in transaction and category display

--- resources/views/content/products.blade.php
+++ resources/views/content/products.blade.php
@@ -30,15 +30,15 @@
                 <ul role="menu" x-show="isOpen" @click.away="isOpen = false"
                     class="absolute mt-2 z-10 min-w-[150px] max-w-[150px] overflow-auto rounded-lg border border-slate-200 bg-main-bg p-1.5 shadow-lg focus:outline-none">
                     <li role="menuitem"
-                        class="cursor-pointer text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-100">
+                        class="cursor-pointer rounded-md text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100">
                         All
                     </li>
                     <li role="menuitem"
-                        class="cursor-pointer text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-100">
+                        class="cursor-pointer rounded-md text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100">
                         Foods
                     </li>
                     <li role="menuitem"
-                        class="cursor-pointer text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100 active:bg-gray-100">
+                        class="cursor-pointer rounded-md text-main-text dark:text-dark-main-text flex w-full text-sm items-center p-3 transition-all hover:bg-gray-100 focus:bg-gray-100">
                         Drinks
                     </li>
                 </ul>
@@ -47,5 +47,21 @@
         <button class="px-3 py-2 min-w-32 bg-secondary-bg hover:bg-secondary-bg/90 rounded-md text-sm text-dark-main-text">
             Add Product
         </button>
+    </div>
+    <div class="relative overflow-x-auto mt-8">
+        <table class="w-full text-sm text-left text-main-text dark:text-dark-main-text">
+            <thead class="px-3 bg-main-bg dark:bg-dark-main-bg text-sm">
+                <tr>
+                    <th class="rounded-s-md px-2 py-3">No</th>
+                    <th class="px-2 py-3">Image</th>
+                    <th class="px-2 py-3">Name</th>
+                    <th class="px-2 py-3">Category</th>
+                    <th class="rounded-e-md px-2 py-3">Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr></tr>
+            </tbody>
+        </table>
     </div>
 @endsection
